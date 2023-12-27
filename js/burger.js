d580a18f1a7d3d072a349d525fd728f5f41c65c3
Nojotel <nojotel@yandex.ru>
2023-12-27 05:05:11 +0300

    ///
    /

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   let menuBtn = document.querySelector(".menu-btn");
   let menu = document.querySelector(".menu");
+  let closeButton = document.querySelector(".close-button--menu");
 
   menuBtn.addEventListener("click", function () {
     menuBtn.classList.toggle("active");
     menu.classList.toggle("active");
+  });
+
+  closeButton.addEventListener("click", function () {
+    menuBtn.classList.remove("active");
+    menu.classList.remove("active");
   });
 });
 
